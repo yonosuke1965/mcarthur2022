1f0769465f9cae390c2a0558d5b7dad4603c65df
yonosuke1965 <46507217+yonosuke1965@users.noreply.github.com>
2025-10-31 12:19:35 +0900

    Oct 31

--- oneliner.rb
+++ oneliner.rb
@@ -1,3 +1,47 @@
+
+while (line = gets)
+  # if /\\([a-z]+?){/ =~ line
+  #   puts $1
+  # end
+  line.gsub!(/\\ig{.*?}/,"")
+  # line.sub!(/\\chapter{(.*)}/, "# \\1")
+  # line.sub!(/\\section{(.*)}/, "## \\1")
+  # line.sub!(/\\subsection{(.*)}/, "## \\1")
+
+  
+  # line.gsub!(/\\citep\[(.*?)\]{(.*?)}/, "\\footnote{\\2, \\1}")
+  # line.gsub!(/\\citep{(.*?)}/, "\\footnote{\\1}")
+  # line.gsub!(/\\citet{(.*?)}/, "\\footnote{\\1}")
+  #    line.gsub!(/\\ruby{(.*?)}{(.*?)}/,"［\\1］［ルビ:\\2］")
+  #    line.gsub!(/\\kenten{(.*?)}/,"［●\\1●］")
+  #    line.gsub!(/\\emph{(.*?)}/,"［*\\1*］")
+  #    line.gsub!(/{\\DDASH}/, "──")
+  #    line.gsub!(/_/, "-")
+     puts line
+
+end
+
+
+
+
+
+__END__
+
+while (line = gets)
+     line.gsub!(/\\ruby{(.*?)}{(.*?)}/,"［\\1］［\\2］")
+     line.gsub!(/\\kenten{(.*?)}/,"［●\\1●］")
+
+    puts line
+end
+
+
+
+
+  
+
+
+__END__
+
 while (input = gets)
 
   input.gsub!(/(\\emph\{[^}]*\})/) do
